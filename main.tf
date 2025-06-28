9eda02c1c849593829f1299b632340d0c25c1f8f
AllainWoodsford <allain.woodsford@studytafensw.edu.au>
2025-06-29 01:10:06 +1000

    Added aws configuration_aliases for each account we have dev test and prod to create seperation of concerns

--- main.tf
+++ main.tf
@@ -8,12 +8,60 @@ terraform {
   }
 }
 
+# Dev Provider Alias
 provider "aws" {
   # Configuration options
+  alias = "dev"
+  region = "ap-southeast-2" #sydney
+
+  assume_role {
+    role_arn = "arn:aws:iam:366483376693:role/OrganizationAccountAccessRole"
+  }
+
   default_tags {
     tags = {
       OwnedBy   = "OilyGooseStudio"
       ManagedBy = "Terraform"
+      Environment = "Dev"
     }
   }
 }
+
+# Test Provider Alias
+provider "aws" {
+  # Configuration options
+  alias = "test"
+  region = "ap-southeast-2" #sydney
+
+  assume_role {
+    role_arn = "arn:aws:iam:253157412494:role/OrganizationAccountAccessRole"
+  }
+
+  default_tags {
+    tags = {
+      OwnedBy   = "OilyGooseStudio"
+      ManagedBy = "Terraform"
+      Environment = "Test"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  alias = "prod"
+  region = "ap-southeast-2" #sydney
+
+  assume_role {
+    role_arn = "arn:aws:iam:430730481777:role/OrganizationAccountAccessRole"
+  }
+
+  default_tags {
+    tags = {
+      OwnedBy   = "OilyGooseStudio"
+      ManagedBy = "Terraform"
+      Environment = "Production"
+    }
+  }
+}
+
+
