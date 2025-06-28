7830c62d0c95a2950d3639ccec7d18869bc45fd2
AllainWoodsford <allain.woodsford@studytafensw.edu.au>
2025-06-29 01:35:03 +1000

    init dev environment prepare to init remote backend

---
+++ dev/main.tf
@@ -0,0 +1,49 @@
+terraform {
+required_version = "~> 1.12.2"
+#   backend "s3" {
+#     bucket         = "input later"
+#     key            = "dev/state/terraform.tfstate"
+#     region         = "ap-southeast-2"
+#     use_lockfile = true
+#     encrypt = true
+#   }
+
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = "~> 6.0.0"
+      configuration_aliases = [aws.dev]
+    }
+  }
+
+  
+}
+
+provider "aws" {
+  # Configuration options
+  alias  = "dev"
+  region = "ap-southeast-2" #sydney
+
+  assume_role {
+    role_arn = "arn:aws:iam:366483376693:role/OrganizationAccountAccessRole"
+  }
+
+  default_tags {
+    tags = {
+      OwnedBy     = "OilyGooseStudio"
+      ManagedBy   = "Terraform"
+      Environment = "Dev"
+    }
+  }
+}
+
+data "aws_caller_identity" "dev" {
+  provider = aws.dev
+}
+
+module "backend" {
+  source = "git@github.com:AllainWoodsford/sports-warehouse-modules.git//s3/remote_backend?ref=v0.0.1"
+
+  #Input Variables
+  bucket_suffix = "dev"
+}
